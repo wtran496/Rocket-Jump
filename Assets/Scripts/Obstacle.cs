97eb92e5e631328f304a131590512eef8bda6d98
wtran496 <wtran0496@csu.fullerton.edu>
2019-08-08 21:23:02 -0700

    Rocket Jump

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System; 
+
+[DisallowMultipleComponent]
+public class Obstacle : MonoBehaviour
+{
+    //Used for rotation movement
+    [SerializeField] float rotationSpeed = 0.0f;
+    [SerializeField] GameObject pivot = null;
+
+    //Used for vector movement
+    [SerializeField] Vector3 movementVector = new Vector3(10f,10f,10f);
+    [SerializeField] float period = 2f;
+    float movementFactor; //0 not moved, 1 moved
+
+    Vector3 startingPos;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pivot)
+            transform.RotateAround(pivot.transform.position, Vector3.forward, rotationSpeed * Time.deltaTime);
+        else
+        {
+            float cycles = Time.time / period; // grow continually from 0
+            const float tau = Mathf.PI * 2;
+            float rawSinWave = Mathf.Sin(cycles * tau);
+            movementFactor = rawSinWave / 2f + .5f;
+            Vector3 offset = movementFactor * movementVector;
+            transform.position = startingPos + offset;
+        }
+    }
+}
+
---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class Rocket : MonoBehaviour
+{
+
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+    Text deathText;
+    [SerializeField] float rcsThrust = 2f;
+    [SerializeField] float mainThrust = 2f;
+    [SerializeField] AudioClip mainEngine = null;
+    [SerializeField] AudioClip finish = null;
+    [SerializeField] AudioClip dead = null;
+    [SerializeField] ParticleSystem mainParticle = null;
+    [SerializeField] ParticleSystem finishParticle = null;
+    [SerializeField] ParticleSystem deadParticle = null;
+    enum State { Alive, Dying, Transcending}
+    State state = State.Alive;
+    bool collisionDisabled = true;
+    private void Thrust()
+    {
+        
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+            if (!audioSource.isPlaying)
+                audioSource.PlayOneShot(mainEngine);
+            mainParticle.Play();
+        }
+        else
+        {
+            audioSource.Stop();
+            mainParticle.Stop();
+        }
+        mainParticle.Play();
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true;
+       
+        float rotationspeed = rcsThrust + Time.deltaTime;
+        if (Input.GetKey(KeyCode.A))
+        { 
+            transform.Rotate(Vector3.forward * rotationspeed);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationspeed);
+        }
+        rigidBody.freezeRotation = false;
+    }
+
+    void OnCollisionEnter(Collision collision) {
+
+        if (state != State.Alive || !collisionDisabled) { return; }
+
+        switch (collision.gameObject.tag) {
+            case "Friendly":
+                print("Friendly");
+                break;
+            case "Finish":
+                state = State.Transcending;
+                audioSource.Stop();
+                audioSource.PlayOneShot(finish);
+                finishParticle.Play();
+                Invoke("loadNext", 1f);
+                print("Finish");
+                break;
+            default:
+                state = State.Dying;
+                audioSource.Stop();
+                audioSource.PlayOneShot(dead);
+                deadParticle.Play();
+                int hi = PlayerPrefs.GetInt("score") + 1; //save death counter
+                PlayerPrefs.SetInt("score", hi);
+                Invoke("loadStart", 1f);
+                print("Dead");
+                break;
+        }
+    }
+
+    private void loadNext() {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings){
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    private void loadStart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); //0
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+
+        deathText = GameObject.Find("Canvas").GetComponentInChildren<Text>();
+        deathText.GetComponent<Text>().text = PlayerPrefs.GetInt("score").ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (state == State.Alive)
+        {
+            Thrust();
+            Rotate();
+        }
+        //RespondToDebugKeys();
+        if (Input.GetKey("escape"))
+        {
+            
+            Application.Quit();
+        }
+    }
+    private void RespondToDebugKeys() {
+        if (Input.GetKeyDown(KeyCode.L)) loadNext();
+        else if (Input.GetKeyDown(KeyCode.C)) collisionDisabled = !collisionDisabled;
+    }
+    void OnApplicationQuit()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+}
+
